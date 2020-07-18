cfe73f3b78bec3f4f9c98bcb8530379689f025f9
Jansen Fink <jansenfink@charter.net>
2020-07-18 19:15:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,11 +6,10 @@ def sort_array_desc(array)
   array.sort.reverse
 end  
 
-def sort_array_char_count(array)
-  array.length.sort 
-end  
+def sort_array_char_count(strings)
+  strings.sort{|a,b| a.length<=>b.length}
+end
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array
-end  
+  array.sort{|a,b| a[1]<=>b[2]}
+end
