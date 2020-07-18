0d6739e51b98a0d1ea758f678912e461f615cc55
Jansen Fink <jansenfink@charter.net>
2020-07-18 19:19:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,6 +18,19 @@ def reverse_array
   array.reverse
 end
 
-def kesha_maker
-array.collect {|array| array.delete(array[2]).insert(2,"$")}
+def kesha_maker(array)
+  array.collect do |arr| arr.delete(arr[2]).insert(2,"$") end
+end
+
+def find_a(array)
+  array.keep_if do |arr| arr[0]=="a" end
+end
+
+def sum_array(array)
+  #same as array.inject(0) do |sum,arr| sum+arr end
+  array.inject(0, :+)
+end
+
+def add_s(array)
+  array.collect do |arr| arr==array[1] ? arr:arr <<"s" end
 end
